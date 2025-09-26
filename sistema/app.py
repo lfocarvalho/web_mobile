4200e2e91205d0b0443a16568f46de749fe99ce1
lfocarvalho <nandooliver120@gmail.com>
2025-09-25 22:12:30 -0300

    atualização sistema

---
+++ sistema/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(
+    __name__,
+    template_folder=os.path.join(os.path.dirname(__file__), '../templates'),
+    static_folder=os.path.join(os.path.dirname(__file__), '../static')
+)
+
+@app.route('/')
+def home():
+    return render_template('autenticacao.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
